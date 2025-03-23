e2e0cc0e4bacab37d8bc646e9cc66b2514a6212b
Yunjin Lee <168180007+yoonjin67@users.noreply.github.com>
2025-03-23 23:09:45 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C11/백준/Bronze/2557. Hello World/Hello World.c
+++ C11/백준/Bronze/2557. Hello World/Hello World.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-#define print(x) main(x)
-#define hello(x) puts(x)
-typedef int world;
-#define string "Hello World!"
-world print(){
-	hello(string);
+main()
+{
+    printf("Hello World!");
 }
