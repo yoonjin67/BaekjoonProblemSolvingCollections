8d51abff290b2552cc5565fd923bab4394f67bd3
Yunjin Lee <168180007+yoonjin67@users.noreply.github.com>
2025-03-28 06:54:18 +0000

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 120356 KB -BaekjoonHub

--- Ruby/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.rb
+++ Ruby/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.rb
@@ -1,10 +1,4 @@
 a = gets.to_i
 for i in 1..a
-    for j in 1..i
-        print "*"
-    end
-    print "\n"
-=begin
-이 부분이 좀 중요함. \n과 같은 특수문자는 double quote 쓰기. 또한 주석은 들여쓰기 불가.
-=end
+        puts"*"*i
 end 
