5c199a6b5236a300de649f069621e8f2c02a8aac
Yunjin Lee <168180007+yoonjin67@users.noreply.github.com>
2025-04-09 14:10:52 +0000

    [Silver V] Title: 좌표 정렬하기, Time: 68 ms, Memory: 2556 KB -BaekjoonHub

---
+++ C11/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<stdio.h>
+typedef struct Tuple{
+    int x, y;
+} tpl;
+int compare(const void *a, const void *b) {
+    tpl A = *(tpl *)a;
+    tpl B = *(tpl *)b;
+    if(A.x!=B.x) {
+        return A.x-B.x;
+    } else {
+        return A.y-B.y;
+    }
+}
+
+
+int main(void) {
+    int N;
+    scanf("%d",&N);
+    tpl arr[N];
+    int i;
+    for(i=0;i<N;i++){
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    }
+    qsort(arr,N,sizeof(tpl),compare);
+    for(i=0;i<N;i++){
+        printf("%d %d\n", arr[i].x, arr[i].y);
+    }
+    return 0;
+}
+    
