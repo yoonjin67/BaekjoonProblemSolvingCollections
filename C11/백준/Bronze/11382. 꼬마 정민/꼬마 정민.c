4e4c22c02bf60bb70dacaf66a7eed70da85794fa
Yunjin Lee <168180007+yoonjin67@users.noreply.github.com>
2025-04-09 15:10:21 +0000

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C11/백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    unsigned long long int first,second,third;
+    scanf("%lld %lld %lld", &first,&second,&third);
+    printf("%lld", first+second+third);
+    return 0;
+}
