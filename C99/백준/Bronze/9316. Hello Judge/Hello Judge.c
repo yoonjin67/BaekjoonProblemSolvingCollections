76e1e000819d76f2d2d51e7969da5c2560172db2
Yunjin Lee <168180007+yoonjin67@users.noreply.github.com>
2025-04-11 14:56:47 +0000

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/9316. Hello Judge/Hello Judge.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define n() main()
+#define nn(x,y) printf(x,y) 
+typedef int nnnn;
+#define m "Hello World, Judge %d!\n"
+nnnn n(){
+    int nnn,N;
+    scanf("%d",&N);
+    for(nnn=1;nnn<=N;nnn++){nn(m,nnn);}
+}
