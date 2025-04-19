6f64326d8b548fbbc10f3e0ddab781d80e98149a
Yunjin Lee <168180007+yoonjin67@users.noreply.github.com>
2025-04-19 18:17:54 +0900

    [Gold II] Title: 피보나치 수 3, Time: 12 ms, Memory: 12832 KB -BaekjoonHub

---
+++ C++17/백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -0,0 +1,26 @@
+#include<stdio.h>
+unsigned long long cache[1500000];
+void fibonacci(int N) {
+    static unsigned long long i = 0;
+    cache[0]=0;
+    cache[1]=1;
+    for(i=2;i<1500000;i++) {
+        cache[i] = (cache[i-1]+cache[i-2])%1000000;
+    }
+}
+
+int main()
+{
+    unsigned long long int N;
+    scanf("%llu", &N);
+    if(N==0) {
+        putchar('0');
+        return 0;
+    } else if (N<2) {
+        putchar('1');
+        return 0;
+    }
+    fibonacci(1500000);
+    printf("%llu", cache[N%1500000]);
+    return 0;
+}
