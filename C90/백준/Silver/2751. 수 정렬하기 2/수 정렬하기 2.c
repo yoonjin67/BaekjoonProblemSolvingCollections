6a50de7c5ce888b5453adec858b0504c0cd3f3fe
Yunjin Lee <168180007+yoonjin67@users.noreply.github.com>
2025-04-19 18:41:27 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 416 ms, Memory: 8808 KB -BaekjoonHub

---
+++ C90/백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int compare(const void *a, const void *b) {
+    return *(const int *)a-*(const int *)b;
+}
+
+int main() {
+    int X, i;
+    /*
+     I am following c90 standard!
+     So All variables should be
+     declared above functions. 
+     */
+    scanf("%d", &X);
+    int array_sort[X];
+    for(i=0;i<X;i++) {
+        scanf("%d",array_sort+i);
+    }
+    
+    qsort(array_sort, X, sizeof(int), compare);
+    
+    for(i=0;i<X;i++) {
+        printf("%d\n", *(array_sort+i));
+    }
+    return 0;
+    
+}
