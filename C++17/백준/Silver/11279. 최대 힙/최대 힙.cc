f1eaabb1799012057714e613fc7846950f2c0782
Yunjin Lee <168180007+yoonjin67@users.noreply.github.com>
2025-04-19 18:19:16 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 2924 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<queue>
+#include<functional>
+using namespace std;
+int main () {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    priority_queue<int,vector<int>,less<int>> q;
+    int rep;
+    scanf("%d",&rep);
+    for(int i=0;i<rep;i++) {
+        int v;
+        scanf("%d",&v);
+        if(v!=0) {
+            q.push(v);
+        }
+        else {
+            if(!q.empty()) {
+                printf("%d\n",q.top());
+                q.pop();
+            } else {
+                puts("0");
+            }
+        }
+    }
+    return 0;
+}
